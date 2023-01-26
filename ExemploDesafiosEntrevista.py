210fea35ef659cd654277b3264f7467036983477
Mauricio Martins <mauriciob.martins@hotmail.com>
2023-01-26 12:42:28 -0300

    py

---
+++ ExemploDesafiosEntrevista.py
@@ -0,0 +1,7 @@
+a = input('Primeiro número: ')
+#entrada para a: 12
+b = input('Segundo número: ')
+#entrada para b: 8
+print(a+b)
+#como nao temos especificado o tipo(str,int,float, etc.
+#ele vai juntar as cedulas como se fossem str por pdrao.
