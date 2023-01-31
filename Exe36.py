accd9f7c130b6ed9aca2346a84e719641cd8085c
Mauricio Martins <mauriciob.martins@hotmail.com>
2023-01-31 11:57:33 -0300

    py

--- Exe36.py
+++ Exe36.py
@@ -1,7 +1,7 @@
 valor = float(input('Digite o valor da casa: R$ '))
 salario = float(input('Digite o slário do comprador: R$ '))
 anos = int(input('Digite em quantos anos pretende se pagar: '))
-
+prestacao = float()
 print('O valor da casa é de {:.2f} e sera paga em {} anos,'
       ' o salário do comprador é de {:.2f}'.format(valor, anos, salario ))
 
