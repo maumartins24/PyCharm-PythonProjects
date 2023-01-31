ecf20f9e6e34cf91109d1e18123921f2375b240d
Mauricio Martins <mauriciob.martins@hotmail.com>
2023-01-31 12:22:31 -0300

    py

--- ex36.py
+++ ex36.py
@@ -1,6 +1,11 @@
 valor = float(input('Digite o valor da casa: R$ '))
 salario = float(input('Digite o slário do comprador: R$ '))
 anos = int(input('Digite em quantos anos pretende se pagar: '))
-prestacao = float()
+prestacao = (valor / (anos * 12))
+minimo = salario * 30 / 10
 print('O valor da casa é de {:.2f} e sera paga em {} anos,'
-      ' o salário do comprador é de {:.2f}'.format(valor, anos, salario ))
+      ' e a prestação sera de {:.2f}'.format(valor, anos, prestacao ))
+if prestacao < minimo:
+    print('O financiamento foi negado!')
+else:
+    print('O financiamento foi aprovado!')
